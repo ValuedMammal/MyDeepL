9e82d78f5f55b26cd1822cc7b57ec92f2d4f2289
Val <valuedmammal@protonmail.com>
2023-07-12 18:08:28 -0400

    debug translate text

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
 #![allow(unused)]
 use deeprl::{
     DeepL,
-    Language, TextOptions, text::SplitSentences,
+    Language, 
+    TextOptions, 
+    text::{SplitSentences, TagHandling},
     text::Formality,
 };
 use std::{
     process, 
     env,
+    str::FromStr,
+    io::{self, Read},
 };
 use anyhow::{Result, Context, bail};
 use crate::args::*;
 pub mod args;
-
-
 
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
@@ -45,11 +47,14 @@ fn main() -> anyhow::Result<()> {
             // src, split_sent, preserve, formal, glos, tags, outline, split_tag, nonsplit_tag, ignore_tag
             let mut opt = TextOptions::new(target_lang);
 
-            if let Some(src) = params.source {
-                let Ok(source_lang) = src.parse::<Language>() else {
-                    bail!("invalid source lang")
-                };
-                opt = opt.source_lang(source_lang);
+            if !params.multi_lang {
+                if let Some(src) = params.source {
+                    let Ok(source_lang) = src.parse::<Language>() else {
+                        bail!("invalid source lang")
+                    };
+                    opt = opt.source_lang(source_lang);
+                }
+                // skip source lang if input contains various langs
             }
 
             if let Some(ss) = params.split_sentences {
@@ -66,7 +71,68 @@ fn main() -> anyhow::Result<()> {
             }
 
             if let Some(f) = params.formality {
-                // opt = opt.formality(Formality::from_str(&f));
+                opt = opt.formality(Formality::from_str(&f).unwrap());
+                // Ok to unwrap, as `from_str` defaults to Default
+            }
+
+            if let Some(g) = params.glossary {
+                opt = opt.glossary_id(g);
+            }
+
+            if let Some(t) = params.tag_handling {
+                let tag = match t.to_lowercase().as_str() {
+                    "xml" => TagHandling::Xml,
+                    "html" => TagHandling::Html,
+                    _ => {
+                        bail!("invalid tag handling");
+                    }
+                };
+                opt = opt.tag_handling(tag);
+            }
+
+            if params.no_outline_detection {
+                opt = opt.outline_detection(false);
+            }
+
+            if let Some(split) = params.splitting_tags {
+                opt = opt.splitting_tags(split);
+            }
+
+            if let Some(non) = params.non_splitting_tags {
+                opt = opt.non_splitting_tags(non);
+            }
+            
+            if let Some(ig) = params.ignore_tags {
+                opt = opt.ignore_tags(ig);
+            }
+
+            // Get input text and call translate api
+            let mut text: Vec<String> = vec![];
+
+            if let Some(t) = params.text {
+                text.push(t);
+                //TODO: if t[0] == '-', read stdin
+            } else {
+                // read stdin
+                let mut s = String::new();
+                io::stdin().read_to_string(&mut s)
+                    .context("failed to read stdin")?;
+
+                // experimental multi-lang input
+                if params.multi_lang {
+                    text = s.split('\n').map(|s| s.to_owned()).collect();
+                } else {
+                    // send a single text param
+                    text.push(s);
+                }
+            }
+
+            let result = dl.translate(opt, text)?;
+            for t in result.translations {
+                if params.show_detected {
+                    println!("Detected: {}", t.detected_source_language);
+                }
+                println!("{}", t.text);
             }
         },
     };
